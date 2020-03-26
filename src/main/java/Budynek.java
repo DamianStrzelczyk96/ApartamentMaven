b040ea1f6041d1d960138b1f575bc638c7ff6b1d
Your Name <damianstrzelczyk@o2.pl>
2020-03-26 13:14:11 +0100

    Blok + DOm

---
+++ src/main/java/Budynek.java
@@ -0,0 +1,9 @@
+public abstract class  Budynek {
+    public abstract int liczbaPokoi();
+    public abstract int liczbaOkien();
+    public abstract double liczbaMetrow();
+    public abstract int liczbaMieszkan();
+    public abstract boolean czyJestGaraż();
+    public abstract boolean czyJestOgródek();
+    public abstract boolean czyJestSilownia();
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,40 +3,46 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
 
-        Apartament warszawa = new Apartament();
-        warszawa.setMiasto("warszawa");
-        warszawa.setCena(150000);
-        warszawa.setPowierzchnia(100);
+//        Apartament warszawa = new Apartament();
+//        warszawa.setMiasto("warszawa");
+//        warszawa.setCena(150000);
+//        warszawa.setPowierzchnia(100);
+//
+//        Apartament krwakow = new Apartament();
+//        krwakow.setMiasto("Krakow");
+//        krwakow.setPowierzchnia(150);
+//        krwakow.setCena(200000);
+//        Apartament gdansk = new Apartament();
+//        gdansk.setCena(300000);
+//        gdansk.setPowierzchnia(200);
+//        gdansk.setMiasto("Gdańsk");
+//
+//        ArrayList<Apartament> mieszkania = new ArrayList();
+//        mieszkania.add(warszawa);
+//        mieszkania.add(krwakow);
+//        mieszkania.add(gdansk);
+//        gdansk.getFullPrice();
+//        System.out.println(gdansk.cena);
+//
+//
+//        System.out.println(mieszkania);
+//        System.out.println(krwakow.getCena());
+//        krwakow.getFullPrice();
+//
+//        double srednia = (warszawa.cena+gdansk.cena+krwakow.cena)/mieszkania.size();
+//        System.out.println(srednia);
+//        System.out.format("%.2f%n", srednia);
+//        double suma = 0;
+//        suma += mieszkania.stream().mapToDouble(Apartament -> Apartament.cena).sum();
+//        double srednia2 = suma/mieszkania.size();
+//        System.out.format("%.2f%n", srednia2);
+        Blok blok1 = new Blok();
+        Dom dom = new Dom();
 
-        Apartament krwakow = new Apartament();
-        krwakow.setMiasto("Krakow");
-        krwakow.setPowierzchnia(150);
-        krwakow.setCena(200000);
-        Apartament gdansk = new Apartament();
-        gdansk.setCena(300000);
-        gdansk.setPowierzchnia(200);
-        gdansk.setMiasto("Gdańsk");
+        System.out.println(blok1);
+        blok1.setPokje(30);
+        System.out.println("zmiana");
+        System.out.println(blok1);
+        System.out.println(dom);
 
-        ArrayList<Apartament> mieszkania = new ArrayList();
-        mieszkania.add(warszawa);
-        mieszkania.add(krwakow);
-        mieszkania.add(gdansk);
-        gdansk.getFullPrice();
-        System.out.println(gdansk.cena);
-
-
-        System.out.println(mieszkania);
-        System.out.println(krwakow.getCena());
-        krwakow.getFullPrice();
-
-        double srednia = (warszawa.cena+gdansk.cena+krwakow.cena)/mieszkania.size();
-        System.out.println(srednia);
-        System.out.format("%.2f%n", srednia);
-        double suma = 0;
-        suma += mieszkania.stream().mapToDouble(Apartament -> Apartament.cena).sum();
-        double srednia2 = suma/mieszkania.size();
-        System.out.format("%.2f%n", srednia2);
-
-        }
-
-}
+}}
